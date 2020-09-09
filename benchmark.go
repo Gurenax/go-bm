c8d3eb902a1ba729ef1e1a18660e7531e0ce6143
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2020-09-10 09:57:00 +1000

    Update function names to capital

--- benchmark.go
+++ benchmark.go
@@ -13,24 +13,24 @@ type Benchmark struct {
 }
 
 // Start a new benchmark
-func (b *Benchmark) startBenchmark() {
+func (b *Benchmark) StartBenchmark() {
 	b.startTime = time.Now()
 }
 
 // End the benchmark then record the duration
-func (b *Benchmark) endBenchmark() {
+func (b *Benchmark) EndBenchmark() {
 	b.endTime = time.Now()
 	b.duration = b.endTime.Sub(b.startTime)
 }
 
 // Display the recorded duration in milliseconds
-func (b *Benchmark) displayResultInMilliseconds(label string) {
+func (b *Benchmark) DisplayResultInMilliseconds(label string) {
 	durationInMs := float64(b.duration.Nanoseconds()) * 0.000001
 	fmt.Printf("%v %fms\n", label, durationInMs)
 }
 
 // Display the recorded duration in seconds
-func (b *Benchmark) displayResultInSeconds(label string) {
+func (b *Benchmark) DisplayResultInSeconds(label string) {
 	durationInSecs := float64(b.duration.Nanoseconds()) * 0.000000001
 	fmt.Printf("%v %fseconds\n", label, durationInSecs)
 }
