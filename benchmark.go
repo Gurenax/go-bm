210b554b079c6f3d6693f95a403ff9393bd75ee8
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2020-09-10 09:50:35 +1000

    Update struct name

--- benchmark.go
+++ benchmark.go
@@ -6,31 +6,31 @@ import (
 )
 
 // Benchmark struct
-type benchmark struct {
+type Benchmark struct {
 	startTime time.Time
 	endTime   time.Time
 	duration  time.Duration
 }
 
 // Start a new benchmark
-func (b *benchmark) startBenchmark() {
+func (b *Benchmark) startBenchmark() {
 	b.startTime = time.Now()
 }
 
 // End the benchmark then record the duration
-func (b *benchmark) endBenchmark() {
+func (b *Benchmark) endBenchmark() {
 	b.endTime = time.Now()
 	b.duration = b.endTime.Sub(b.startTime)
 }
 
 // Display the recorded duration in milliseconds
-func (b *benchmark) displayResultInMilliseconds(label string) {
+func (b *Benchmark) displayResultInMilliseconds(label string) {
 	durationInMs := float64(b.duration.Nanoseconds()) * 0.000001
 	fmt.Printf("%v %fms\n", label, durationInMs)
 }
 
 // Display the recorded duration in seconds
-func (b *benchmark) displayResultInSeconds(label string) {
+func (b *Benchmark) displayResultInSeconds(label string) {
 	durationInSecs := float64(b.duration.Nanoseconds()) * 0.000000001
 	fmt.Printf("%v %fseconds\n", label, durationInSecs)
 }
