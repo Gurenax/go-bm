15df40f834a1c69b1bed0fa222500005e7558bc0
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2020-09-15 10:58:50 +1000

    Add tests for micro and nano secs

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,6 +1,9 @@
 package benchmark
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -71,6 +74,104 @@ func TestBM_End(t *testing.T) {
 			}
 			if b.duration == 0 {
 				t.Errorf("Expected duration to be initialised, but got %v", b.duration)
+			}
+		})
+	}
+}
+
+func TestBM_DurationInNanoseconds(t *testing.T) {
+	var givenDuration time.Duration = 1000000000
+
+	type fields struct {
+		startTime time.Time
+		endTime   time.Time
+		duration  time.Duration
+	}
+	type args struct {
+		label string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "Executes successfully",
+			fields: fields{
+				duration: givenDuration,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+
+			b := &BM{
+				startTime: tt.fields.startTime,
+				endTime:   tt.fields.endTime,
+				duration:  tt.fields.duration,
+			}
+			b.DurationInNanoseconds(tt.args.label)
+
+			w.Close()
+			out, _ := ioutil.ReadAll(r)
+			os.Stdout = mockStdout
+
+			actualResult := strings.Trim(string(out), " \n")
+			expectedResult := "1000000000.000000ns"
+			if actualResult != expectedResult {
+				t.Errorf("Expected %v, got %v", expectedResult, actualResult)
+			}
+		})
+	}
+}
+
+func TestBM_DurationInMicroseconds(t *testing.T) {
+	var givenDuration time.Duration = 1000000000
+
+	type fields struct {
+		startTime time.Time
+		endTime   time.Time
+		duration  time.Duration
+	}
+	type args struct {
+		label string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "Executes successfully",
+			fields: fields{
+				duration: givenDuration,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+
+			b := &BM{
+				startTime: tt.fields.startTime,
+				endTime:   tt.fields.endTime,
+				duration:  tt.fields.duration,
+			}
+			b.DurationInMicroseconds(tt.args.label)
+
+			w.Close()
+			out, _ := ioutil.ReadAll(r)
+			os.Stdout = mockStdout
+
+			actualResult := strings.Trim(string(out), " \n")
+			expectedResult := "1000000.000000µs"
+			if actualResult != expectedResult {
+				t.Errorf("Expected %v, got %v", expectedResult, actualResult)
 			}
 		})
 	}
